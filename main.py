9c720916bee304ab608e0a795bdebfd45764ed6f
Hang <1145900873@qq.com>
2020-09-15 14:39:27 +0800

    利用余弦相似度计算文本相似度模块完成

--- main.py
+++ main.py
@@ -1,13 +1,68 @@
+from __future__ import division
 import sys
 import re
+import jieba.analyse
+from functools import reduce
+from math import sqrt
+
+
+# 用余弦相似度计算文本相似度
+class Similarity:
+    # 初始化
+    def __init__(self, target1, target2, topK=1000):
+        self.vdict2 = {}
+        self.vdict1 = {}
+        self.target1 = target1
+        self.target2 = target2
+        self.topK = topK
+
+    def vector(self):
+        # 利用jieba进行分词并返回对应的权重
+        top_keywords1 = jieba.analyse.extract_tags(self.target1, topK=self.topK, withWeight=True)
+        top_keywords2 = jieba.analyse.extract_tags(self.target2, topK=self.topK, withWeight=True)
+        # 分别生成字典
+        for k, v in top_keywords1:
+            self.vdict1[k] = v
+        for k, v in top_keywords2:
+            self.vdict2[k] = v
+
+    def mix(self):
+        for key in self.vdict1:
+            self.vdict2[key] = self.vdict2.get(key, 0)
+        for key in self.vdict2:
+            self.vdict1[key] = self.vdict1.get(key, 0)
+
+        def mapminmax(vdict):
+            """计算相对词频"""
+            _min = min(vdict.values())
+            _max = max(vdict.values())
+            _mid = _max - _min
+            # print _min, _max, _mid
+            for key2 in vdict:
+                vdict[key2] = (vdict[key2] - _min) / _mid
+            return vdict
+
+        self.vdict1 = mapminmax(self.vdict1)
+        self.vdict2 = mapminmax(self.vdict2)
+
+    # 利用余弦相似度计算文本相似度
+    def similar(self):
+        self.vector()
+        self.mix()
+        sum2 = 0
+        for key in self.vdict1:
+            sum2 += self.vdict1[key] * self.vdict2[key]
+        A = sqrt(reduce(lambda x, y: x + y, map(lambda x: x * x, self.vdict1.values())))
+        B = sqrt(reduce(lambda x, y: x + y, map(lambda x: x * x, self.vdict2.values())))
+        return round(sum2 / (A * B), 2)
 
 
 # 输入模块(命令行执行)
 def main():
     test_ad = sys.argv[1]
     copy_ad = sys.argv[2]
-    # answer_ad = sys.argv[3]
-    # answer = open(answer_ad, 'w')
+    answer_ad = sys.argv[3]
+    answer = open(answer_ad, 'w')
     data1 = []
     # 以utf-8格式，打开要测试的文件并按行读取拼接到data2列表中
     for line in open(test_ad, 'r', encoding='utf-8'):
@@ -33,9 +88,14 @@ def main():
     punc = '~`!#$%^&*()_+-=|\';":/.,?><~·！@#￥%……&*（）——+-=“”：’；、。，？》《{}'
     test1 = re.sub(r'[%s]+' % punc, '', test1)
     test2 = re.sub(r'[%s]+' % punc, '', test2)
-    print(test1)
-    print(test2)
+
+    topK = 1000
+    s = Similarity(test1, test2, topK)
+    total = s.similar()
+    print(total)
+    answer.write(str(total))
+    return 0
 
 
-main()
-# 用余弦相似度计算文本相似度
+if __name__ == '__main__':
+    main()
